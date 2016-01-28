a21d7a9580e61888898eea69053406df9f3c4f87
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-01-29 01:25:28 +0700

    Add Android toolchain example

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -5,10 +5,23 @@ set -x
 if [ "`uname`" == "Darwin" ];
 then
   brew install python3
+  if [[ "${TOOLCHAIN}" == android-* ]];
+  then
+    http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin
+    chmod +x android-ndk-r10e-darwin-x86_64.bin
+    ./android-ndk-r10e-darwin-x86_64.bin
+  fi
 fi
 
 if [ "`uname`" == "Linux" ];
 then
   wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz
   tar xf cmake-3.4.0-Linux-x86_64.tar.gz
+
+  if [[ "${TOOLCHAIN}" == android-* ]];
+  then
+    wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+    chmod +x android-ndk-r10e-linux-x86_64.bin
+    ./android-ndk-r10e-linux-x86_64.bin
+  fi
 fi
